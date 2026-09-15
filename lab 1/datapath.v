41f4a378205a6fb740f22163acb7d6fc0dc72235
rsarang14 <hunter14tom@gmail.com>
2026-09-15 17:28:22 +0530

    Update datapath.v

--- lab 1/datapath.v
+++ lab 1/datapath.v
@@ -22,8 +22,6 @@ module datapath (
     reg [3:0] working_multiplicand;
     reg [3:0] loop_counter;
     
-    // APR (8-bit)
-    // EC23I2015
     always @(posedge clk) begin
         if (clear_prod == 1'b1) begin
             accumulated_prod <= 8'd0;
@@ -33,24 +31,22 @@ module datapath (
         end
     end
 
-    // Working Multiplicand Register (4-bit)
     always @(posedge clk) begin
         if (load_inputs == 1'b1) begin
             working_multiplicand <= multiplicand_in;
         end
     end
-
-    //4 bit Down Loop Counter 
+ 
     always @(posedge clk) begin
         if (load_inputs == 1'b1) begin
             loop_counter <= multiplier_in;
         end
         else if (enable_cnt_dec == 1'b1) begin
-            loop_counter <= loop_counter - 1; // Or loop_counter - 4'd1
+            loop_counter <= loop_counter - 1; 
         end
     end
 
-    // Zero Detector
+    
     assign loops_zero_flag = (loop_counter == 4'd0);
 
 endmodule
